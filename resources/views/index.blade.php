d7c0794a9721aa79a1b7e08998f0c1ce838f5399
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2023-07-10 22:43:36 -0300

    feat: Adding Vue and tailwind.css to the project.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Laravel React App</title>
+    <!-- Include the compiled JavaScript and CSS files for your React app -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app"></div>
+
+    <!-- Include the compiled JavaScript bundle for your React app -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
