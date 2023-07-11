6302e5ed5b0f9a0c52e8bfaf0c6295539572db49
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2023-07-10 23:06:24 -0300

    feat: Creating Models and Controllers for posts and comments.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts;
+
 
 class PostsController extends Controller
 {
@@ -10,5 +12,24 @@ class PostsController extends Controller
     public function index()
     {
         return view('posts');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        Posts::create($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post created successfully.');
     }
 }
