2821f235d57f648979869d15ec203e8c0714377f
Paulo Junior Schüssler <paulojuniorpjs@hotmail.com>
2023-07-10 21:19:03 -0300

    feat: Creating PostsController and adding React.js to the project.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInitacd70f4c915b663e4641eae01e93c3c7
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInitacd70f4c915b663e4641eae01e93c3c7::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInitacd70f4c915b663e4641eae01e93c3c7::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireacd70f4c915b663e4641eae01e93c3c7($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequireacd70f4c915b663e4641eae01e93c3c7($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
